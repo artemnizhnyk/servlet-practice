bd6f6a4bd0518cc9bfae874a057690ea7f889801
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-08-30 17:32:00 +0200

    UserInfo entity was added

--- src/main/java/com/artemnizhnyk/servletpractice/domain/model/UserInfo.java
+++ src/main/java/com/artemnizhnyk/servletpractice/domain/model/UserInfo.java
@@ -1,4 +1,30 @@
 package com.artemnizhnyk.servletpractice.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_info")
+@Entity
 public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String firstname;
+    @Column(unique = true, nullable = false)
+    private String surname;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(unique = true, nullable = false)
+    private String city;
+    @Column(unique = true, nullable = false)
+    private String street;
+    @Column(unique = true, nullable = false)
+    private String postalCode;
 }
